0f84f6aee37441203ac7b8f314fe83340e1b7f78
dev-Adward <youchanj66@gmail.com>
2023-07-28 16:26:51 +0900

    파이썬 장고 공부중 화면에 출력하는 것까지 완료

--- study_django/main/models.py
+++ study_django/main/models.py
@@ -1,3 +1,17 @@
+from typing import Any
 from django.db import models
 
 # Create your models here.
+
+class Member(models.Model):
+    name = models.CharField(max_length=100)
+    # end_date = models.DateTimeField()
+    area = models.CharField(max_length=15)
+    
+    
+    def to_json(self):
+        return {
+            "name" : self.name,
+            # "end_date" : self.end_date.isoformat,
+            "area" : self.area,
+        }
--- study_django/main/views.py
+++ study_django/main/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
+from . import models
+import json
 
 
-# 장고가 url을 연결하는 방식
+# 장고가 url을 연결하는 방식, 컨트롤러와 매우 유사함
 def main(request):
-    print(request)  
+    
+    jsonArray = []
+    member = models.Member.objects.get(pk=1)
+    member_json = json.dumps(member.to_json())
+    # json.dumps(member.to_json)
+    
+    # for element in models.Member.objects.all():
+    #     print(element)
+        # jsonObject = json.dumps(element)
+        # jsonArray.append(jsonObject)
+    
     # 요청이 들어오면 templates안에 main안에 main.html파일을 선택하겠다.
-    return render(request, "main/main.html")
+    return render(request, "main/main.html", {"member":member_json})
+
